0befc457a566f0f78b0d727f0647f6ba17d30aa0
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-11-04 09:09:32 +0100

    Add GameState bindings to Lua
    They don't contain anything (yet), but are required because
    System::init takes a GameState parameter.

--- src/engine/script_bindings.cpp
+++ src/engine/script_bindings.cpp
@@ -4,6 +4,7 @@
 #include "engine/component_factory.h"
 #include "engine/engine.h"
 #include "engine/entity.h"
+#include "engine/game_state.h"
 #include "engine/serialization.h"
 #include "engine/system.h"
 #include "engine/touchable.h"
@@ -19,6 +20,7 @@ thrive::EngineBindings::luaBindings() {
         ComponentFactory::luaBindings(),
         Entity::luaBindings(),
         Touchable::luaBindings(),
+        GameState::luaBindings(),
         Engine::luaBindings()
     );
 }
